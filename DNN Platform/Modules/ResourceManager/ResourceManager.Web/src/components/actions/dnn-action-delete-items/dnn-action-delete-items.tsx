30668ca0debd22f7bc356ed0c3cc73f9c9d38059
Mahmoud Nouraei <m.noraei@gmail.com>
2022-10-08 12:28:36 +0330

    resource manager rtl

--- DNN Platform/Modules/ResourceManager/ResourceManager.Web/src/components/actions/dnn-action-delete-items/dnn-action-delete-items.tsx
+++ DNN Platform/Modules/ResourceManager/ResourceManager.Web/src/components/actions/dnn-action-delete-items/dnn-action-delete-items.tsx
@@ -31,7 +31,9 @@ export class DnnActionDeleteItems {
       <Host>
         <button onClick={() => this.handleClick()}>
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg>
-          <span>{state.localization.Delete}</span>
+          {/* START persian-dnnsoftware */}
+          { document.documentElement.lang.toLowerCase()==='fa-ir'? <span style={{fontFamily:"IRANSans"}}>{state.localization.Delete}</span>:<span>{state.localization.Delete}</span>}
+          {/* END persian-dnnsoftware */}
         </button>
       </Host>
     );
--- DNN Platform/Modules/ResourceManager/ResourceManager.Web/src/components/actions/dnn-action-download-item/dnn-action-download-item.tsx
+++ DNN Platform/Modules/ResourceManager/ResourceManager.Web/src/components/actions/dnn-action-download-item/dnn-action-download-item.tsx
@@ -26,7 +26,9 @@ export class DnnActionDownloadItem {
       <Host>
         <button onClick={() => this.handleClick()}>
           <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path xmlns="http://www.w3.org/2000/svg" d="M12 16 7 11 8.4 9.55 11 12.15V4H13V12.15L15.6 9.55L17 11ZM6 20Q5.175 20 4.588 19.413Q4 18.825 4 18V15H6V18Q6 18 6 18Q6 18 6 18H18Q18 18 18 18Q18 18 18 18V15H20V18Q20 18.825 19.413 19.413Q18.825 20 18 20Z"/></svg>
-          <span>{state.localization.Download}</span>
+          {/* START persian-dnnsoftware */}
+          { document.documentElement.lang.toLowerCase()==='fa-ir'? <span style={{fontFamily:"IRANSans"}}>{state.localization.Download}</span>:<span>{state.localization.Download}</span>}
+          {/* END persian-dnnsoftware */}
         </button>
       </Host>
     );
